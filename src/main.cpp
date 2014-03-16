2e417f4d6bdc49a60f9daeee568708229350d9ed
Pablo Recio <pablo@potatolondon.com>
2014-03-16 09:35:08 +0000

    Super simple data structure definition, everything on the same file

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,56 @@
 #include <iostream>
+#include <time.h>
+
+
+class Board {
+public:
+    int grid[4][4];
+
+    Board() {
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                grid[i][j] = 0;
+            }
+        }
+
+        random_tile();
+        random_tile();
+    }
+
+private:
+
+    void random_tile() {
+        int col = rand() % 4;
+        int row = rand() % 4;
+
+        if (grid[col][row] == 0) {
+            grid[col][row] = generate_tile();
+        } else {
+            random_tile();
+        }
+    }
+
+    int generate_tile() {
+        int value = 2;
+
+        if ((rand() % 10) < 1) {
+            value = 4;
+        }
+        return value;
+    }
+};
+
 
 int main() {
-    std::cout << "Hello World!";
+    // setup random seed
+    srand(time(NULL));
+
+    Board b;
+
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 4; j++) {
+            std::cout << b.grid[i][j] << ", ";
+        }
+        std::cout << std::endl;
+    }
 }
